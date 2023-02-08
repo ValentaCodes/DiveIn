e0f6d2934802af24a90a23338db340574672513e
ValentaCodes <valentacodes@gmail.com>
2023-02-07 21:31:05 -0700

    Added location routes FEB 7 2023

--- controllers/api/locationRoute.js
+++ controllers/api/locationRoute.js
@@ -1,6 +1,48 @@
-const router = require('express').Router()
-const {Location} = require('../../models')
+const router = require("express").Router();
+const { Location, Boat } = require("../../models");
 
 // This is the '/api/location' endpoint
 
-router.post('/')
+// Create a location boat using api
+router.post("/", async (req, res) => {
+  try {
+    const locationData = await Location.create(req.body);
+    res.status(200).json(locationData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+// Delete a location using api
+router.delete("/:id", async (req, res) => {
+  try {
+    const locationData = await Location.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!locationData) {
+      res.status(400).json({ message: "Unable to find location with this id" });
+    }
+    res.status(200).json(locationData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// get all locations including boats at docked there
+router.get("/", async (req, res) => {
+  try {
+    const locationData = await Location.findAll({
+      include: {
+        model: Boat,
+        attributes: ["title", "availability"],
+      },
+    });
+    res.status(200).json(locationData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
