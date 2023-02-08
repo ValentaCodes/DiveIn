2463feaf67929551c9b0eb83b07c0b856a5350a5
ValentaCodes <valentacodes@gmail.com>
2023-02-08 08:18:10 -0700

    uncommented location routes for use FEB 8 2023

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 // const renterRoutes = require("./renterRoute");
-// const locationRoutes = require("./locationRoute");
+const locationRoutes = require("./locationRoute");
 const boatRoutes = require("./boatRoute");
 
-// router.use("/location", locationRoutes);
+router.use("/location", locationRoutes);
 // router.use("/renter", renterRoutes);
 router.use("/boat", boatRoutes);
 
