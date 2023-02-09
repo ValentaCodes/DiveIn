81c312e5fe79e998563692102595d59e68bb4b88
ValentaCodes <valentacodes@gmail.com>
2023-02-09 00:59:02 -0700

    added a renter_id coloumn so we can establish connection between two tables FEB 9 2023

--- models/Boat.js
+++ models/Boat.js
@@ -29,16 +29,24 @@ Boat.init(
     },
     availability: {
       type: DataTypes.BOOLEAN,
-      allowNull: false
+      allowNull: false,
     },
     // NOTE: added image table so we can store boat images
     image: {
-      type: DataTypes.BLOB('long')
+      type: DataTypes.BLOB("long"),
     },
     location_id: {
       type: DataTypes.INTEGER,
       references: {
         model: "location",
+        key: "id",
+      },
+    },
+    // added this so we can create a connection between boat and renter. now we know what boat is rented by who
+    renter_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "renter",
         key: "id",
       },
     },
