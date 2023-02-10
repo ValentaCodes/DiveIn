0dd7f22776199167efea0b9e4de826a5f63d4688
ValentaCodes <valentacodes@gmail.com>
2023-02-09 21:29:56 -0700

    added functionality to create new user FEB 9 2023

--- controllers/api/renterRoute.js
+++ controllers/api/renterRoute.js
@@ -7,41 +7,35 @@ const { Renter } = require("../../models");
 // CREATE a new Renter
 router.post("/", async (req, res) => {
   try {
-    const dbRenterData = await Renter.create({
-      Rentername: req.body.Rentername,
-      email: req.body.email,
-      password: req.body.password,
-      password: req.body.phone,
-    });
+    const renterData = await Renter.create(req.body);
 
     req.session.save(() => {
-      req.session.loggedIn = true;
-
-      res.status(200).json(dbRenterData);
+      req.session.renter_id = renterData.id;
+      req.session.logged_in = true;
+      res.status(200).json(renterData);
     });
   } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
 // Login
 router.post("/login", async (req, res) => {
   try {
-    const dbRenterData = await Renter.findOne({
+    const renterData = await Renter.findOne({
       where: {
-        email: req.body.email,
+        username: req.body.username,
       },
     });
 
-    if (!dbRenterData) {
+    if (!renterData) {
       res.status(400).json({
-        message: "The email or password do not match. Please try again.",
+        message: "The username or password do not match. Please try again.",
       });
       return;
     }
 
-    const validPassword = await dbRenterData.checkPassword(req.body.password);
+    const validPassword = renterData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res.status(400).json({
@@ -51,21 +45,19 @@ router.post("/login", async (req, res) => {
     }
 
     req.session.save(() => {
-      req.session.loggedIn = true;
+      req.session.renter_id = renterData.id;
+      req.session.logged_in = true;
 
-      res
-        .status(200)
-        .json({ Renter: dbRenterData, message: "You are now logged in!" });
+      res.json({ renter: renterData, message: "You are now logged in!" });
     });
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
 
 // Logout
 router.post("/logout", (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
     });
