2710e99e77a7855029298b2388af2310ef90b17a
ValentaCodes <valentacodes@gmail.com>
2023-02-15 21:46:57 -0700

    removed nodemon

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const sequelize = process.env.JAWSDB_URL
       process.env.DB_USER,
       process.env.DB_PASSWORD,
       {
-        host: '0.0.0.0',
+        host: 'localhost',
         dialect: "mysql",
         port: 3306,
       }
