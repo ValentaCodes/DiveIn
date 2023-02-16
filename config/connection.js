a99b339418e472d9cfc8185b4e9db695f750cc2b
ValentaCodes <valentacodes@gmail.com>
2023-02-15 22:09:51 -0700

    removed JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,12 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
+const sequelize =  new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
       process.env.DB_PASSWORD,
       {
-        host: '127.0.0.0',
+        host: 'localhost',
         dialect: "mysql",
         port: 3306,
       }
