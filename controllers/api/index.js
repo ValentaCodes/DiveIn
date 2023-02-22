855336987994780eff85fc6e05b3b77a307c8c36
ValentaCodes <valentacodes@gmail.com>
2023-02-22 00:40:21 -0700

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 const renterRoutes = require("./renterRoute");
 const locationRoutes = require("./locationRoute");
 const boatRoutes = require("./boatRoute");
+<<<<<<< HEAD
 // Routes for our API
+=======
+
+>>>>>>> parent of afbac71 (Merge)
 router.use("/location", locationRoutes);
 router.use("/renter", renterRoutes);
 router.use("/boat", boatRoutes);
