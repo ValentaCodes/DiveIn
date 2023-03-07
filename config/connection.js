d870383c44f8472cb35bb397286fc0fe0d58381d
ValentaCodes <valentacodes@gmail.com>
2023-03-06 21:45:55 -0700

    changedDB

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,13 @@ require("dotenv").config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
+      process.env.MYSQLDATABASE,
+      process.env.MYYSQLUSER,
+      process.env.MYSQLPASSWORD,
       {
-        host: 'localhost',
+        host: process.env.MYSQLHOST,
         dialect: "mysql",
-        port: 3306,
+        port: process.env.MYSQLPORT,
       }
     );
 
