ac29ece511d269367ae01bb4f9802d7529e6caa9
ValentaCodes <valentacodes@gmail.com>
2023-03-06 21:51:11 -0700

    fixed typo

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 // Connect to heroku db if not run these variables and connect
 const sequelize = new Sequelize(
-      process.env.MYSQLDATABASE,
-      process.env.MYYSQLUSER,
-      process.env.MYSQLPASSWORD,
-      {
-        host: process.env.MYSQLHOST,
-        dialect: "mysql",
-        port: process.env.MYSQLPORT,
-      }
-    );
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    dialect: "mysql",
+    port: process.env.MYSQLPORT,
+  }
+);
 
 module.exports = sequelize;
